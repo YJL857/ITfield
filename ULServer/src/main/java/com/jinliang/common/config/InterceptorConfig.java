b2d6b4d571166714613f8bbc9f7a086a07ccf444
yejinliang <Andy19951220@163.com>
2022-07-03 02:44:49 +0800

    异常处理

--- ULServer/src/main/java/com/jinliang/common/config/InterceptorConfig.java
+++ ULServer/src/main/java/com/jinliang/common/config/InterceptorConfig.java
@@ -1,6 +1,6 @@
-package com.jinliang.config;
+package com.jinliang.common.config;
 
-import com.jinliang.interceptor.UserLoginInterceptor;
+import com.jinliang.common.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,10 +15,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new UserLoginInterceptor())
+        registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/**")
-                .excludePathPatterns("/error", "/user/**", "/categories", "/products", "/products/*", "/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**",
-                        "/api", "/api-docs", "/api-docs/**", "/service-worker.js/**", "/doc.html/**");
+                .excludePathPatterns("/error", "/user/login", "/user/createUser","/swagger-resources/**", "/webjars/**", "/swagger-ui.html/**",
+                        "/api", "/api-docs", "/api-docs/**", "/service-worker.js/**", "/doc.html/**", "/favicon.ico");
         //写好的拦截器需要通过这里添加注册才能生效
         WebMvcConfigurer.super.addInterceptors(registry);
     }
--- ULServer/src/main/java/com/jinliang/common/interceptor/LoginInterceptor.java
+++ ULServer/src/main/java/com/jinliang/common/interceptor/LoginInterceptor.java
@@ -1,4 +1,4 @@
-package com.jinliang.interceptor;
+package com.jinliang.common.interceptor;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,33 +8,61 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
+ * 登录检查
+ * 1、配置好拦截器要拦截哪些请求
+ * 2、把这些配置放在容器中
  * @author yejinliang
  * @create 2022-06-23 1:03
  */
-public class UserLoginInterceptor implements HandlerInterceptor {
-    /*@Override
+public class LoginInterceptor implements HandlerInterceptor {
+    /**
+     * 目标方法执行之前
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+       // 检查登录逻辑
         HttpSession session = request.getSession();
-        Object reglister = session.getAttribute("Reglister");
-
-
+        Object reglister = session.getAttribute("loginUser");
         if (reglister != null) {
             return true;
         } else {
             request.setAttribute("msg", "请先登录!");
-
             request.getRequestDispatcher("/").forward(request, response);
             return false;
         }
     }
 
+    /**
+     * 目标方法执行之后
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
+    /**
+     * 前端渲染之后
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
-    }*/
+    }
 }
